e788bd07420b1eb422f36f856ec650e5fda85e4d
SouBelaExpediçao <viniciusfrancisco326@gmail.com>
2026-05-22 19:53:02 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,1 +1,20 @@
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
+firebase.initializeApp({
+  apiKey: "AIzaSyAB09oj91RuaytI8AIfFrXc1mYESnGPr9o",
+  authDomain: "sistema-soubela.firebaseapp.com",
+  projectId: "sistema-soubela",
+  storageBucket: "sistema-soubela.firebasestorage.app",
+  messagingSenderId: "625696420183",
+  appId: "1:625696420183:web:7d264c60c9d2fd4cbcce12"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: 'Sou bela -logo.png'
+  });
+});
